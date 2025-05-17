1767e456d95af14b490a4276429edb0b88cb1ff1
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-05-17 20:10:44 +0900

    Fix judgment size

--- play/src/engine/playData/archetypes/Judgment.js
+++ play/src/engine/playData/archetypes/Judgment.js
@@ -1,6 +1,7 @@
 import { skin, getZ, layer } from '../skin.js';
 import { NormalLayout } from '../../../../../shared/src/engine/data/utils.js';
 import { options } from '../../configuration/options.js';
+import { ease } from '../../../../../shared/src/engine/data/EaseType.js';
 
 export class Judg extends SpawnableArchetype({
     j: Number,
@@ -10,15 +11,12 @@ export class Judg extends SpawnableArchetype({
     endTime = this.entityMemory(Number);
     layout = this.entityMemory(Quad);
     z = this.entityMemory(Number);
+    combo = levelMemory(Number);
+    comboc = this.entityMemory(Number);
+    check = this.entityMemory(Boolean)
+    ratio = this.entityMemory(Number);
     initialize() {
-        debug.log(this.spawnData.j)
-        this.endTime = this.spawnData.t + 1;
-        NormalLayout({
-            l: -0.3,
-            r: 0.3,
-            t: 0.85,
-            b: 0.75,
-        }).copyTo(this.layout);
+        this.endTime = this.spawnData.t + 0.3;
         this.z = getZ(layer.judgment, -this.spawnData.t, 0);
     }
     updateParallel() {
@@ -26,24 +24,72 @@ export class Judg extends SpawnableArchetype({
             this.despawn = true;
             return;
         }
+        if (this.comboc != this.combo) {
+            this.despawn = true;
+            return;
+        }
         if (options.customJudgment) {
-            const a = Math.unlerp(this.endTime, this.spawnData.t, time.now);
+            const targetAspectRatio = 16 / 9;
+            const stage = {
+                w: options.lockStageAspectRatio
+                    ? screen.aspectRatio >= targetAspectRatio
+                        ? targetAspectRatio * screen.h
+                        : screen.w
+                    : screen.w,
+                h: options.lockStageAspectRatio
+                    ? screen.aspectRatio >= targetAspectRatio
+                        ? screen.h
+                        : screen.w / targetAspectRatio
+                    : screen.h,
+            };
             switch (this.spawnData.j) {
                 case Judgment.Perfect:
-                    skin.sprites.perfect.draw(this.layout, this.z, a);
+                    this.ratio = 3.83;
                     break;
                 case Judgment.Great:
-                    skin.sprites.great.draw(this.layout, this.z, a);
+                    this.ratio = 3.49;
                     break;
                 case Judgment.Good:
-                    skin.sprites.good.draw(this.layout, this.z, a);
+                    this.ratio = 2.65;
                     break;
                 case Judgment.Miss:
-                    skin.sprites.miss.draw(this.layout, this.z, a);
+                    this.ratio = 2.23;
+                    break;
+            }
+            const h = stage.h * 0.05 * ui.configuration.judgment.scale
+            const w = h * this.ratio * 5.5
+            const centerX = 0
+            const centerY = stage.h * 0.4
+            const s = Math.min(1, Math.unlerp(this.spawnData.t, this.spawnData.t + 0.07, time.now));
+            NormalLayout({
+                l: centerX - (w * s) / 2,
+                r: centerX + (w * s) / 2,
+                t: centerY - (h * s) / 2,
+                b: centerY + (h * s) / 2,
+            }).copyTo(this.layout);
+            switch (this.spawnData.j) {
+                case Judgment.Perfect:
+                    skin.sprites.perfect.draw(this.layout, this.z, 1);
+                    break;
+                case Judgment.Great:
+                    skin.sprites.great.draw(this.layout, this.z, 1);
+                    break;
+                case Judgment.Good:
+                    skin.sprites.good.draw(this.layout, this.z, 1);
+                    break;
+                case Judgment.Miss:
+                    skin.sprites.miss.draw(this.layout, this.z, 1);
                     break;
             }
         }
         /*if (options.customCombo) {
         }*/
+    }
+    updateSequential() {
+        if (!this.check) {
+            this.combo += 1
+            this.comboc = this.combo
+        }
+        this.check = true
     }
 }
