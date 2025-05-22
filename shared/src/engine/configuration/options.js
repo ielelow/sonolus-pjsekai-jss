9eed09f1ac8e17a42c1ff6c1b3f54359c4b3b09f
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-05-22 17:00:08 +0900

    Lower stage cover step

--- shared/src/engine/configuration/options.js
+++ shared/src/engine/configuration/options.js
@@ -152,7 +152,7 @@ export const optionsDefinition = {
         def: 0,
         min: 0,
         max: 1,
-        step: 0.05,
+        step: 0.01,
         unit: Text.PercentageUnit,
     },
     lockStageAspectRatio: {
