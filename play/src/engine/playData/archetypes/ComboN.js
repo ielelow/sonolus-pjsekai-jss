b253dc4f208ba4a523bad6afafe83d66d053575a
ielelow <ieleee1107@gmail.com>
2025-05-29 14:46:06 +0800

    Update ComboN.js

--- play/src/engine/playData/archetypes/ComboN.js
+++ play/src/engine/playData/archetypes/ComboN.js
@@ -43,15 +43,15 @@ export class ComboN extends SpawnableArchetype({
         if (digits[0] === 0) digitCount = 3;
         if (digits[0] === 0 && digits[1] === 0) digitCount = 2;
         if (digits[0] === 0 && digits[1] === 0 && digits[2] === 0) digitCount = 1;
-        this.ratio = 0.773;
-        const h = 0.14 * ui.configuration.combo.scale
-        const centerX = 5.15
-        const centerY = 0.58
+        this.ratio = 0.71889;
+        const h = 0.1624 * ui.configuration.combo.scale
+        const centerX = 5.36
+        const centerY = 0.59
         // 애니메이션 = s * (원래좌표) + (1 - s) * centerX, s * (원래좌표) + (1 - s) * centerY
         const s = 0.6 + 0.4 * Math.ease('Out', 'Cubic', Math.min(1, Math.unlerp(this.spawnData.t, this.spawnData.t + 0.15, time.now)))
         const a = ui.configuration.combo.alpha * Math.ease('Out', 'Cubic', Math.min(1, Math.unlerp(this.spawnData.t, this.spawnData.t + 0.15, time.now)))
-        const digitWidth = h * this.ratio * 6.65
-        const digitGap = digitWidth * -0.05;
+        const digitWidth = h * this.ratio * 7.5145
+        const digitGap = digitWidth * -0.04;
         const totalWidth = digitCount * digitWidth + (digitCount - 1) * digitGap;
         const startX = centerX - totalWidth / 2;
         if (digitCount === 1) {
