44c150e26d0f97827072397e8bfb8fa368c077c2
ielelow <ieleee1107@gmail.com>
2025-05-29 10:17:07 +0800

    Update ComboN.js

--- watch/src/engine/watchData/archetypes/ComboN.js
+++ watch/src/engine/watchData/archetypes/ComboN.js
@@ -47,13 +47,13 @@ export class ComboN extends SpawnableArchetype({
             if (digits[0] === 0 && digits[1] === 0) digitCount = 2;
             if (digits[0] === 0 && digits[1] === 0 && digits[2] === 0) digitCount = 1;
             const h = 0.14 * ui.configuration.combo.scale
-            const centerX = 5.15
-            const centerY = 0.58
+            const centerX = 5.36
+            const centerY = 0.32
             // 애니메이션 = s * (원래좌표) + (1 - s) * centerX, s * (원래좌표) + (1 - s) * centerY
             const s = 0.6 + 0.4 * Math.ease('Out', 'Cubic', Math.min(1, Math.unlerp(this.spawnData.t, this.spawnData.t + 0.15, time.now)))
             const a = ui.configuration.combo.alpha * Math.ease('Out', 'Cubic', Math.min(1, Math.unlerp(this.spawnData.t, this.spawnData.t + 0.15, time.now)))
-            const digitWidth = h * 0.773 * 6.65
-            const digitGap = digitWidth * -0.05;
+            const digitWidth = h * 0.773 * 7.5145
+            const digitGap = digitWidth * -0.0565;
             const totalWidth = digitCount * digitWidth + (digitCount - 1) * digitGap;
             const startX = centerX - totalWidth / 2;
             if (digitCount === 1) {
