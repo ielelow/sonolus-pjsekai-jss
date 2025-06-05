48501a9bada515021b8b6705ddd46c4d9af8ec7d
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-06-06 00:53:16 +0900

    Update options.js

--- shared/src/engine/configuration/options.js
+++ shared/src/engine/configuration/options.js
@@ -59,7 +59,7 @@ export const optionsDefinition = {
         scope: 'Sekai',
         advanced: true,
         type: 'slider',
-        def: 0.15,
+        def: 0.10,
         min: -0.5,
         max: 0.5,
         step: 0.01,
