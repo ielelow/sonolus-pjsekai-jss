284b7cd7112f3e49fa2e30a4519a175ff60be1c1
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-06-10 16:33:40 +0900

    Fix minor bug

--- play/src/engine/playData/archetypes/slideConnectors/activeSlideConnectors/SlideParticleManager.js
+++ play/src/engine/playData/archetypes/slideConnectors/activeSlideConnectors/SlideParticleManager.js
@@ -1,25 +1,23 @@
 import { archetypes } from "../../index";
 export class SlideParticleManager extends SpawnableArchetype({
     startRef: Number,
-    t: Number,
+    function: Number,
 })
 {
     check = this.entityMemory(Boolean)
-    spawnTime() {
-        return timeScaleChanges.at(this.spawnData.t).scaledTime;
-    }
-    despawnTime() {
-        return timeScaleChanges.at(this.spawnData.t + 1).scaledTime;
-    }
     terminate() {
         this.check = false
     }
     updateSequential() {
-        if (this.check)
-            return
+        if (this.check) {
+            this.despawn = true;
+            return;
+        }
         this.check = true
-        this.startSharedMemory.circular = 0
-        this.startSharedMemory.linear = 0
+        if (this.spawnData.function == 0)
+            this.startSharedMemory.circular = 0
+        if (this.spawnData.function == 1)
+            this.startSharedMemory.linear = 0
     }
     get startSharedMemory() {
         return archetypes.NormalSlideStartNote.sharedMemory.get(this.spawnData.startRef);
