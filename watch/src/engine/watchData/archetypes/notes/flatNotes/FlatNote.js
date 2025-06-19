1dbce7c553b347f900ea7513d62a570c4d8ef891
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-06-19 21:21:10 +0900

    Fix scaletime error

--- watch/src/engine/watchData/archetypes/notes/flatNotes/FlatNote.js
+++ watch/src/engine/watchData/archetypes/notes/flatNotes/FlatNote.js
@@ -38,7 +38,7 @@ export class FlatNote extends Note {
             Range.l.mul(note.duration).add(timeScaleChanges.at(this.targetTime).scaledTime),
         )
         this.sharedMemory.despawnTime =
-            this.visualTime.min - timeScaleChanges.at(this.hitTime).scaledTime <= 0.016
+            timeScaleChanges.at(this.hitTime - 0.016).scaledTime <= this.visualTime.min
                 ? timeScaleChanges.at(this.hitTime + 0.016).scaledTime
                 : timeScaleChanges.at(this.hitTime).scaledTime
         if (options.sfxEnabled) {
