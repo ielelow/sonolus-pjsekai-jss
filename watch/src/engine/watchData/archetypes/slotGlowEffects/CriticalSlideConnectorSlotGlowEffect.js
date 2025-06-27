fd95ea647b325c7c6cd70989ea44fe6c2a625b1e
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-06-28 08:48:01 +0900

    Clean code

--- watch/src/engine/watchData/archetypes/slotGlowEffects/CriticalSlideConnectorSlotGlowEffect.js
+++
@@ -1,5 +0,0 @@
-import { skin } from '../../skin.js'
-import { SlotGlowEffect } from './SlotGlowEffect.js'
-export class CriticalSlideConnectorSlotGlowEffect extends SlotGlowEffect {
-    sprite = skin.sprites.criticalSlideConnectorSlotGlow
-}
