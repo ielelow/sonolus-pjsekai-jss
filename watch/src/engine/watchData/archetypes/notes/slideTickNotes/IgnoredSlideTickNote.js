cff16f5682a27204768241f80f31a68c597e9fc2
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-07-15 18:36:50 +0900

    Fix incorrect guide note display in mirror mode.

--- watch/src/engine/watchData/archetypes/notes/slideTickNotes/IgnoredSlideTickNote.js
+++ watch/src/engine/watchData/archetypes/notes/slideTickNotes/IgnoredSlideTickNote.js
@@ -1,5 +1,9 @@
 import { SlideTickNote } from './SlideTickNote.js'
+import { options } from '../../../../configuration/options.js'
 export class IgnoredSlideTickNote extends SlideTickNote {
     hasInput = false
-    preprocess() {}
+    preprocess() {
+        if (options.mirror) this.import.lane *= -1
+        this.targetTime = bpmChanges.at(this.import.beat).time
+    }
 }
