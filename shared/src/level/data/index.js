d142d46394e1c62f2510fdf8248d1a8e488ba835
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-07-31 14:33:56 +0900

    Remove Dubug Level

--- shared/src/level/data/index.js
+++ shared/src/level/data/index.js
@@ -1,7 +1,5 @@
 import chart from './append.json'
 import sus from './append.txt'
-import chart2 from './append2.json'
 import { susToUSC } from '../../../../lib/src/index'
-import { uscToUSC } from '../../../../lib/src/index'
 import { uscToLevelData } from '../../../../lib/src/index'
-export const data = chart2
+export const data = uscToLevelData(susToUSC(sus))
