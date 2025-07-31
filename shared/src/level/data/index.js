076b61dd110a98c2de1433dbd31fd6fb4e697da8
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-07-31 09:20:25 +0900

    Change Debug song

--- shared/src/level/data/index.js
+++ shared/src/level/data/index.js
@@ -1,2 +1,5 @@
 import chart from './append.json'
-export const data = chart
+import sus from './append.txt'
+import { susToUSC } from '../../../../lib/src/index'
+import { uscToLevelData } from '../../../../lib/src/index'
+export const data = uscToLevelData(susToUSC(sus))
