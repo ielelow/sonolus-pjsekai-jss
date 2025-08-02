7b95a2c275acb443399cf4d61bbf71f1701e849e
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-08-02 10:30:09 +0900

    Update 1.2.8

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -5,7 +5,7 @@ export { uscToLevelData } from "./usc/convert.js";
 export * from "./usc/index.js";
 export { uscToUSC } from "./usc/revert.js";
 
-export const version = "1.3.7";
+export const version = "1.3.8";
 
 export const databaseEngineItem = {
     name: "prosekaR",
